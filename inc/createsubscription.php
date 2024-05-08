ddce755699433c90cd277742591c496aa2d36e2d
Krishna Btist <krishnabtist@gmail.com>
2024-05-08 12:53:00 +0530

    testing uninstall
    testing uninstall

--- inc/createsubscription.php
+++ inc/createsubscription.php
@@ -76,41 +76,66 @@ function create_subscription_entry($order_id) {
 }
 
 add_action('wp_head',function(){
-    // Dummy data
-    $dummy_data = array(
-        array(
-            'order_id'        => 1,
-            'order_date'      => '2024-05-08',
-            'time_duration'   => '1 year',
-            'user_id'         => 1,
-            'product_details' => 'Dummy product 1',
-        ),
-        array(
-            'order_id'        => 2,
-            'order_date'      => '2024-05-09',
-            'time_duration'   => '6 months',
-            'user_id'         => 2,
-            'product_details' => 'Dummy product 2',
-        ),
-    );
 
-    // Insert dummy data
-    // foreach ($dummy_data as $data) {
-    //     $post_args = array(
-    //         'post_title'   => 'Order #' . $data['order_id'],
-    //         'post_content' => '',
-    //         'post_status'  => 'publish',
-    //         'post_type'    => 'subscription_wp360', // Replace with your custom post type
-    //         'meta_input'   => array(
-    //             '_order_id'        => $data['order_id'],
-    //             '_order_date'      => $data['order_date'],
-    //             '_time_duration'   => $data['time_duration'],
-    //             '_user_id'         => $data['user_id'],
-    //             '_product_details' => $data['product_details'],
+
+            // Delete post meta data
+        // $wp360_subscription_post_ids = get_posts( array(
+        //     'post_type' => 'subscription_wp360', 
+        //     'fields'    => 'ids',
+        //     'posts_per_page' => -1,
+        // ));
+        // foreach ( $wp360_subscription_post_ids as $wp360_subscription_post_id ) {
+
+        //     echo $wp360_subscription_post_id . 'POST ID <br>';
+        // }
+    // Dummy data
+    // $postID = 44;
+    // update_post_meta($postID ,'_wp360_subscription_product' , 'check meta deleted or not');
+    // echo "TEST ECHO ".get_post_meta($postID , '_wp360_subscription_product' , true);
+    // $dummy_data = array(
+    //     array(
+    //         'order_id'        => 1,
+    //         'order_date'      => '2024-05-08',
+    //         'time_duration'   => '1 year',
+    //         'user_id'         => 1,
+    //         'product_details' => array(
+    //             'product_id'   => 44,
+    //             'quantity'     => 1,
+    //             'price'        => '100',
+    //             'time_duration' => 'monthly',
     //         ),
-    //     );
-    //     $post_id = wp_insert_post($post_args);
-    // }
+    //     ),
+    //     array(
+    //         'order_id'        => 2,
+    //         'order_date'      => '2024-05-09',
+    //         'time_duration'   => '6 months',
+    //         'user_id'         => 2,
+    //         'product_details' => array(
+    //             'product_id'   => 45,
+    //             'quantity'     => 1,
+    //             'price'        => '100',
+    //             'time_duration' => 'monthly',
+    //         ),
+    //     ),
+    // );
+
+    // // Insert dummy data
+    // // foreach ($dummy_data as $data) {
+    // //     $post_args = array(
+    // //         'post_title'   => 'Order #' . $data['order_id'],
+    // //         'post_content' => '',
+    // //         'post_status'  => 'publish',
+    // //         'post_type'    => 'subscription_wp360', // Replace with your custom post type
+    // //         'meta_input'   => array(
+    // //             '_order_id'        => $data['order_id'],
+    // //             '_order_date'      => $data['order_date'],
+    // //             '_time_duration'   => $data['time_duration'],
+    // //             '_user_id'         => $data['user_id'],
+    // //             '_product_details' => $data['product_details'],
+    // //         ),
+    // //     );
+    // //     $post_id = wp_insert_post($post_args);
+    // // }
 
 
 });
